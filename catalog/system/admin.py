014797eb9ccbffce33f3056bc82c32c721e3baf1
Michal Jasko <cojeti@potom.com>
2023-11-25 15:34:58 +0100

    created database models

--- catalog/system/admin.py
+++ catalog/system/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from system.models import (Attribute, AttributeName, AttributeValue, Product,
+                           ProductAttributes, Image, ProductImage, Catalog)
 
-# Register your models here.
+class ProductAttributesAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Attribute)
+admin.site.register(AttributeName, search_fields=['nazev', 'kod'])
+admin.site.register(AttributeValue, search_fields=['hodnota'])
+admin.site.register(Product, search_fields=['nazev', 'description'])
+admin.site.register(ProductAttributes, ProductAttributesAdmin)
+admin.site.register(Image)
+admin.site.register(ProductImage)
+admin.site.register(Catalog, search_fields=['nazev'])
--- catalog/system/models.py
+++ catalog/system/models.py
@@ -1,3 +1,75 @@
 from django.db import models
 
-# Create your models here.
+
+class AttributeName(models.Model):
+    nazev = models.CharField(max_length=100)
+    kod = models.CharField(max_length=100, null=True)
+    zobrazit = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nazev
+
+
+class AttributeValue(models.Model):
+    hodnota = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.hodnota
+
+
+class Attribute(models.Model):
+    nazev_atributu = models.ForeignKey(AttributeName, on_delete=models.CASCADE)
+    hodnota_atributu = models.ForeignKey(AttributeValue, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.nazev_atributu} - {self.hodnota_atributu}'
+
+
+class Product(models.Model):
+    nazev = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+    cena = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    mena = models.CharField(max_length=10, default="CZK")
+    published_on = models.DateTimeField(null=True)
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nazev
+
+
+class ProductAttributes(models.Model):
+    attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.product} - {self.attribute}'
+
+    class Meta:
+        verbose_name_plural = "Product attributes"
+
+
+class Image(models.Model):
+    nazev = models.CharField(max_length=100, null=True)
+    obrazek = models.URLField(max_length=255)
+
+    def __str__(self):
+        return self.obrazek
+
+
+class ProductImage(models.Model):
+    nazev = models.CharField(max_length=100)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    obrazek = models.ForeignKey(Image, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.product} - {self.nazev}'
+
+
+class Catalog(models.Model):
+    nazev = models.CharField(max_length=100)
+    obrazek = models.ForeignKey(Image, on_delete=models.SET_NULL, null=True)
+    products = models.ManyToManyField(Product, blank=True)
+    attributes = models.ManyToManyField(Attribute, blank=True)
+
+    def __str__(self):
+        return self.nazev
