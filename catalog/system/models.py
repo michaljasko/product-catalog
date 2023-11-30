d3fe5e06f3eb81d26ea7ebc7817bd78584c8bf8f
Michal Jasko <cojeti@potom.com>
2023-11-30 21:46:03 +0100

    renamed model fields

--- catalog/system/models.py
+++ catalog/system/models.py
@@ -2,39 +2,39 @@ from django.db import models
 
 
 class AttributeName(models.Model):
-    nazev = models.CharField(max_length=100)
-    kod = models.CharField(max_length=100, null=True)
-    zobrazit = models.BooleanField(default=False)
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=100, null=True)
+    is_visible = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.nazev
+        return self.name
 
 
 class AttributeValue(models.Model):
-    hodnota = models.CharField(max_length=100)
+    value = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.hodnota
+        return self.value
 
 
 class Attribute(models.Model):
-    nazev_atributu = models.ForeignKey(AttributeName, on_delete=models.CASCADE)
-    hodnota_atributu = models.ForeignKey(AttributeValue, on_delete=models.CASCADE)
+    attribute_name = models.ForeignKey(AttributeName, on_delete=models.CASCADE)
+    attribute_value = models.ForeignKey(AttributeValue, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.nazev_atributu} - {self.hodnota_atributu}'
+        return f'{self.attribute_name} - {self.attribute_value}'
 
 
 class Product(models.Model):
-    nazev = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     description = models.TextField(null=True)
-    cena = models.DecimalField(max_digits=12, decimal_places=2, default=0)
-    mena = models.CharField(max_length=10, default="CZK")
+    price = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    currency = models.CharField(max_length=10, default="CZK")
     published_on = models.DateTimeField(null=True)
     is_published = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.nazev
+        return self.name
 
 
 class ProductAttributes(models.Model):
@@ -49,29 +49,29 @@ class ProductAttributes(models.Model):
 
 
 class Image(models.Model):
-    nazev = models.CharField(max_length=100, null=True)
-    obrazek = models.URLField(max_length=255)
+    name = models.CharField(max_length=100, null=True)
+    image_url = models.URLField(max_length=255)
 
     def __str__(self):
-        if self.nazev:
-            return f'{self.obrazek} ({self.nazev})'
-        return self.obrazek
+        if self.name:
+            return f'{self.image_url} ({self.name})'
+        return self.image_url
 
 
 class ProductImage(models.Model):
-    nazev = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    obrazek = models.ForeignKey(Image, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.product} - {self.nazev}'
+        return f'{self.product} - {self.name}'
 
 
 class Catalog(models.Model):
-    nazev = models.CharField(max_length=100)
-    obrazek = models.ForeignKey(Image, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=100)
+    image = models.ForeignKey(Image, on_delete=models.SET_NULL, null=True)
     products = models.ManyToManyField(Product, blank=True)
     attributes = models.ManyToManyField(Attribute, blank=True)
 
     def __str__(self):
-        return self.nazev
+        return self.name
